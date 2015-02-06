3aca0a6213167ff20f69bf1137cb7696e7b0e545
Gavin Noronha <gavinln@hotmail.com>
2015-02-05 21:33:06 -0800

    Created an Angular app for sentry raven error logging

---
+++ web/angular/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vagrantApp
+ * @description
+ * # vagrantApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vagrantApp', []);
---
+++ web/angular/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vagrantApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vagrantApp
+ */
+angular.module('vagrantApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    //var temp = Math();
+  });
